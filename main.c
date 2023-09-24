93dbd9f94ff814a6ece0f938017ced4e15e90e35
Igor Michael <igormichael120@gmail.com>
2023-09-24 20:13:35 -0300

    tests executed

--- main.c
+++ main.c
@@ -7,32 +7,41 @@
 #include "lib/kmeans.h"
 #define MAXCHAR 1000
 
-int main() {
+int main(int argc, char* argv[]) {
+    int no_iteration = strtol(argv[1], NULL, 10);
     int clusters = 6;
+    double processing_time;
+    clock_t start, finish;
     // Read dataset and assign the elements to struct.
     struct CSVFile csvData = readCsv("data/genres_v2.csv");
     struct KMeans model = {
+        .no_iteration = no_iteration,
         .no_clusters = clusters,
         .centroids = malloc(sizeof(float) * csvData.columns * clusters),
         .columns = csvData.columns,
         .rows = csvData.rows,
         .data = csvData.data,
         .data_clusters = malloc(sizeof(int) * csvData.rows)};
+
     // Initialize the model with random centroids.
     init_model(&model);
+
     // Computing time taken to run the function.
-    time_t t;
-    t = time(NULL);
+    start = clock();
+
     // Run the KMeans clustering algorithm on data points.
     fit(&model);
+
     // Display the results.
     // printCentroids(&model);
     // printClusterCount(&model);
-    time_t end = time(NULL);
-    double time_taken = difftime(end, t);
-    printf("\n\nThe program took %f seconds to execute.\n\n", time_taken);
 
-    char *resFile = "cluster_results_serial.csv";
+    finish = clock();
+    processing_time = ((double)(finish - start) / CLOCKS_PER_SEC);
+
+    printf("\nThe program took %f seconds to processing.\n", processing_time);
+
+    char* resFile = "cluster_results_serial.csv";
     writeToCSV(&model, resFile);
     return 0;
 }
